1095a017534581cdb3abf0f719d74c2dc4c6fac7
Bailey Bryant <bailey.bryant10@gmail.com>
2017-10-07 22:49:53 -0500

    add MyCart component

---
+++ src/components/MyCart.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import inventory from "../Data/inventory.js";
+
+
+
+
+export default class MyCart extends Component {
+
+
+  render() {
+
+    return (
+      <div>
+      <h1>View Cart</h1>
+      <hr></hr>
+      <p>Save items</p>;
+      </div>
+    )
+  }
+}
